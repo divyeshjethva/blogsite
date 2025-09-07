d47887ddec34d7ada8da0280e6fbec1a8caafcda
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-07 14:22:21 +0530

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    bio = models.CharField(default="",max_length=255)
+    profile = models.ImageField(default="",upload_to='profile/')
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+class CreateBlog(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    name =  models.CharField(max_length=100)
+    desc = models.CharField(max_length=255)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    comment = models.IntegerField(default=0)
+    image = models.ImageField(default="",upload_to='blog/')
+    ttime = models.DateTimeField(default=timezone.now())
+    
+    def __str__(self):
+        return f"{self.name}  |  {self.user}"
---
+++ myapp/views.py
@@ -0,0 +1,140 @@
+from django.shortcuts import render,redirect
+from .models import *
+
+# # Create your views here.
+# def layout(request):
+#     user = User.objects.get(email=request.session['email'])
+#     return render(request,'layout.html',{'user':user})
+    
+def index(request):
+    blog = CreateBlog.objects.all()
+    if request.session.get('email') :
+        user = User.objects.get(email=request.session['email'])
+        return render(request,'index.html',{'blog':blog,'user':user})
+    else:
+        return render(request,'index.html',{'blog':blog})
+
+def about(request):
+    if request.session.get('email') :
+        user = User.objects.get(email=request.session['email'])
+        return render(request,'about.html',{'user':user})
+    else:
+        return render(request,'about.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        try:
+            user = User.objects.get(email=request.POST['email'])
+            if user:
+                msg = "user Already Exists"
+                return render(request,'signup.html',{'msg':msg})
+        except:
+            if request.POST['password'] == request.POST['cpassword']:
+                User.objects.create(
+                    name = request.POST['name'],
+                    email = request.POST['email'],
+                    password = request.POST['password'],
+                )
+                return redirect('login')
+            else:
+                msg = "Password does not match"
+                return render(request,'signup.html',{'msg':msg})
+    else:
+        return render(request,'signup.html')
+
+def login(request):
+    if request.method == "POST":
+        try:
+            user = User.objects.get(email = request.POST['email'])
+            if user.password == request.POST['password']:
+                request.session['email'] = request.POST['email']
+                return redirect('index')
+            else:
+                msg = "password does not match"
+                return render(request,'login.html',{'msg':msg})
+        except:
+            msg = "user Not Found"
+            return render(request,'login.html',{'msg':msg})
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    del request.session['email']
+    try:
+        del request.session['profile']
+    except:
+        print("prpfile not found")
+    return redirect('login')
+
+def contect(request):
+    if request.session.get('email'):
+        user = User.objects.get(email=request.session['email'])
+        return render(request,'contect.html',{'user':user})
+    else:
+        return render(request,'contect.html')
+
+def profile(request):
+    user = User.objects.get(email = request.session['email'])
+    blog = CreateBlog.objects.filter(user=user)
+    return render(request,'profile.html',{'user':user,'blog':blog})
+
+def addblog(request):
+    if request.method == "POST":
+        user = User.objects.get(email = request.session['email'])  
+        CreateBlog.objects.create(
+            user = user,
+            name = request.POST['name'],
+            desc = request.POST['desc'],
+            image = request.FILES['image']
+        )
+        return redirect('profile')
+    else:
+        return render(request,'addblog.html')
+
+def editprofile(request):
+    user = User.objects.get(email = request.session['email'])
+    if request.method == "POST":
+        user.name = request.POST['name']
+        user.bio = request.POST['bio']
+        try:
+            user.profile = request.FILES['profile']
+        except:
+            print("image not gound")
+        user.save()
+        request.session['profile'] = user.profile.url
+        return redirect('profile')
+    else:
+        return render(request,'editprofile.html',{'user':user})
+    
+def editblog(request,pk):
+    user = User.objects.get(email = request.session['email'])
+    blog = CreateBlog.objects.get(pk=pk)
+    if request.method == "POST":
+        blog.name = request.POST['name']
+        blog.desc = request.POST['desc']
+        try:
+            blog.image = request.FILES['image']
+        except:
+            print("image not gound")
+        blog.save()
+        return redirect('profile')
+    else:
+        return render(request,'editblog.html',{'blog':blog})
+    
+def deleteblog(request,pk):
+    user = User.objects.get(email = request.session['email'])
+    blog = CreateBlog.objects.get(pk=pk)
+    blog.delete()
+    return redirect('profile')
+    
+def blogdetails(request,pk):
+    try:
+        user = User.objects.get(email = request.session['email'])
+        blog = CreateBlog.objects.get(pk=pk)
+        return render(request,'blogdetails.html',{'blog':blog,'user':user})
+    except:
+        return redirect('login')
+
+def wishlist(request):
+    return render(request,'wishlist.html')
